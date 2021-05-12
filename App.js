e4745cfffee33b37d6ba580f92aeb10dd5cdba86
oal2727 <joseluis_1555@hotmail.com>
2021-05-12 18:44:11 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  View
+} from 'react-native';
+import PhoneSignIn from "./src/PhoneAuth"
+
+
+const App = () => {
+
+  return (
+   <View>
+     <PhoneSignIn/>
+     </View>
+  );
+};
+
+
+export default App;
